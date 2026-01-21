9d1625cd766b89918e0b6e01ab876d06a08431a1
mm1441 <mihaelmajetic141@gmail.com>
2026-01-21 13:58:49 +0100

    Delete WidgetModule

--- widgets/android/src/main/java/com/magicmarinac/tasks/widget/WidgetModule.kt
+++
@@ -1,9 +0,0 @@
-class WidgetModule : Module(appContext) {
-    override fun definition() = ModuleDefinition {
-        Function("updateWidget") {
-            val intent = Intent("com.example.tasksapp.UPDATE_WIDGET")
-            intent.component = ComponentName(appContext.reactContext!!, "com.example.tasksapp.widget.TasksWidgetProvider")
-            appContext.reactContext!!.sendBroadcast(intent)
-        }
-    }
-}
